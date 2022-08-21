495714a316b5edf916f8dd7cfba4233082a04ee8
Combatsa <59173977+CombtSa@users.noreply.github.com>
2022-08-22 01:13:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import donnes
 import fonctions
-#TypeError: '<' not supported between instances of 'int' and 'str'
 
 
 
 def verif_listmot():
     if donnes.list_mots.count(donnes.list_mots) == 8:
         print("Le mot a trouve est validé")
-    elif donnes.list_mots.count(donnes.list_mots) < 8:
-        print("Probleme valeur superieur")
-        print("Fin du programme")
-        quit()
+        fonctions.trouver()
     else:
         print("Erreur")
+        quit()
 
-
+print(donnes.nom)
 print(donnes.list_mots)
 verif_listmot()
-fonctions.trouver()
